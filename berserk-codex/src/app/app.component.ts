480fe14dbca5caf559d85104e3d7fcdcdf3d8195
lilHmidou <ahmedaminesahnoun@gmail.com>
2025-02-02 15:48:35 +0100

    premiere implémentation - page d'accueil - need clean

---
+++ berserk-codex/src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  isDarkMode = false;
+
+  toggleTheme() {
+    this.isDarkMode = !this.isDarkMode;
+    const theme = this.isDarkMode ? 'dark' : 'light';
+    document.documentElement.setAttribute('data-bs-theme', theme);
+  }
+}
+
