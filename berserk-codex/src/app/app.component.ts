91e92805136b54ecfb914cfa4c37ad1dd85334d3
lilHmidou <ahmedaminesahnoun@gmail.com>
2025-02-02 17:32:06 +0100

    Ajustement des composants de l'application

--- berserk-codex/src/app/app.component.ts
+++ berserk-codex/src/app/app.component.ts
@@ -1,19 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  template: `
+    <app-navbar></app-navbar>
+    <app-home></app-home>
+    <app-footer></app-footer>
+  `,
+  standalone: true,
+  imports: [NavbarComponent, HomeComponent, FooterComponent],
 })
 export class AppComponent {
-  isDarkMode = false;
-
-  toggleTheme() {
-    this.isDarkMode = !this.isDarkMode;
-    const theme = this.isDarkMode ? 'dark' : 'light';
-    document.documentElement.setAttribute('data-bs-theme', theme);
-  }
+  title = 'berserk-codex';
 }
 
--- berserk-codex/src/app/app.routes.ts
+++ berserk-codex/src/app/app.routes.ts
@@ -1,7 +1,8 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
+import { HomeComponent } from './components/home/home.component';
 
 export const routes: Routes = [
-    { path: '', component: HomeComponent }, 
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
+  { path: 'home', component: HomeComponent },  
   ];
   
