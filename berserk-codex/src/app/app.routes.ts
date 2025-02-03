72e92a7dfffc54e87dc9631f10f13012eeb9a528
snow <snowyle22@gmail.com>
2025-02-03 15:53:17 +0100

    routes mises a jours

--- berserk-codex/src/app/app.routes.ts
+++ berserk-codex/src/app/app.routes.ts
@@ -3,9 +3,13 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { MapComponent } from './components/map/map.component';
 import { ArcFriezeComponent } from './components/arc-frieze/arc-frieze.component';
+import {EncyclopediaComponent} from './components/encyclopedie/encyclopedie.component';
+import {ArmesArtifactsComponent} from './components/armes-artifacts/armes-artifacts.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'map', component: MapComponent},
+  { path: 'encyclopedie', component: EncyclopediaComponent},
+  { path: 'armes-artifacts', component: ArmesArtifactsComponent},
   { path:'arc-frieze', component:ArcFriezeComponent}
 ];
